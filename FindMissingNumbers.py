d38d649267a51a5bbf8d11a9e5dfaba992410f2c
Sats17 <satishkumbhar9999@gmail.com>
2020-08-02 22:50:48 +0530

    Added FindMissingNumber Linear algo

---
+++ FindMissingNumbers.py
@@ -0,0 +1,21 @@
+"""
+Logic - Iterate over the array, get difference between currentIndex and nextIndex. if difference is not
+equal to zero then iterate currentIndex + 1 till less than next index and append all values in missing array.s
+"""
+
+def findMissingNumbers(arr, start, end):
+    missingValues = []
+    for i in range(0, len(arr) - 1):
+        diff = arr[i+1] - arr[i]
+        if diff != 0:
+            missing = arr[i] + 1
+            while missing < arr[i+1]:
+                missingValues.append(missing)
+                missing += 1
+    return missingValues
+
+
+arr = [1, 2, 4, 6, 7, 12, 20]
+start = 1
+end = 20
+print(findMissingNumbers(arr, start, end))
