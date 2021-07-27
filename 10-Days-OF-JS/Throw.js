0b0fa863d6644145a638c982989a3bf994b576d5
sats17 <satishkumbhar9999@gmail.com>
2021-07-27 20:05:11 +0530

    Added JS Files of 10-Days-JS event

---
+++ 10-Days-OF-JS/Throw.js
@@ -0,0 +1,17 @@
+function isPositive(a) {
+    if(a > 0){
+        return "YES"
+    }
+    else if(a < 0){
+        throw new Error('Negative Error')
+    }
+    else{
+        throw new Error('Zero Error')
+    }
+}
+
+function main(){
+    isPositive(-2)
+}
+
+main()
