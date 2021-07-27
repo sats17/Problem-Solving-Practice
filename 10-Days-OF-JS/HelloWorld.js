42b45435a60e4b810f5527493cacaabb33357036
sats17 <satishkumbhar9999@gmail.com>
2021-07-27 12:36:36 +0530

    Added Hello World of JS

---
+++ 10-Days-OF-JS/HelloWorld.js
@@ -0,0 +1,16 @@
+'use strict';
+
+main();
+
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+**/
+function greeting() {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');    
+}
+
+
+function main() {
+    greeting();
+}
---
+++ LongestCommonSubsequence.py
@@ -0,0 +1,11 @@
+# Flow
+"""
+metadata 
+str1 = string 1 = a,b,c,d,b
+str2 = string 2 = c,e,d,b
+1) loop character i < str1.len or j < str2.len
+2) j = str1[i], k = str2[i]
+3) if i == j, then i = i + 1 and j = j + 1
+4) if i != j, j = j + 1
+5) 
+"""
