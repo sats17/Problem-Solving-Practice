d585ef1aa81451df188d9e9fcf1d46f3cf2b3838
sats17 <satishkumbhar9999@gmail.com>
2021-11-05 16:28:16 +0530

    Zipped payment data

--- ProblemSolving/BillSplit.py
+++ ProblemSolving/BillSplit.py
@@ -9,12 +9,15 @@ function return - Will return result about who have to pay whom and how much
 def getPaymentExchangeInformation(totalMembers, expense):
     print("Total members ", totalMembers)
     print("Expense data ", expense)
-    totalBills = calculateTotalBill(expense)
-    print("Total bill ", totalBills)
-    # contriBillForEachMember = calculateEachMemberContribution(totalBill, totalMembers)
-    # print("Each members contri should be ", contriBillForEachMember)
-    # totalPayFromEachMemberForEachSource = calculateTotalPaymentFromEachMembers(expense, totalMembers)
-    # print("Total members payment for each source ", totalPayFromEachMemberForEachSource)
+    totalBill = calculateTotalBill(expense)
+    print("Total bill ", totalBill)
+    expectedContriBillForEachMember = calculateExpectedContributionFromEachMember(totalBill, totalMembers)
+    print("Each members contri should be ", expectedContriBillForEachMember)
+    totalPayFromEachMemberForEachSource = calculateTotalPaymentFromEachMembers(expense, totalMembers)
+    print("Total members payment for each source ", totalPayFromEachMemberForEachSource)
+    names = getNamesFromExpenses(expense)
+    zippedPaymentData = zipPaymentAndNames(names, totalPayFromEachMemberForEachSource)
+    differntiateWhoCanPayWhom(zippedPaymentData, expectedContriBillForEachMember)
 
     return 0
 
@@ -26,7 +29,7 @@ def calculateTotalBill(expense):
     return totalBill
 
 
-def calculateEachMemberContribution(totalBill, member):
+def calculateExpectedContributionFromEachMember(totalBill, member):
     return totalBill / member
 
 
@@ -34,15 +37,45 @@ def calculateTotalPaymentFromEachMembers(expense, totalMembers):
     totalMembersPayment = []
     tempArrayForTotalPayment = []
     for i in expense:
-        tempArrayForTotalPayment.append(expense.get(i))
-    print(tempArrayForTotalPayment)
+        tempPayment = []
+        payments = expense.get(i).get('payments')
+        for j in payments:
+            tempPayment.append(payments.get(j))
+        tempArrayForTotalPayment.append(tempPayment)
     zippedPaymentOfEachMembers = zip(*tempArrayForTotalPayment)
     for i in zippedPaymentOfEachMembers.__iter__():
         eachPayment = 0
         for j in i:
-            eachPayment = eachPayment + j;
+            eachPayment = eachPayment + j
         totalMembersPayment.append(eachPayment)
     return totalMembersPayment
+
+
+def zipPaymentAndNames(names, payment):
+    zippedData = {}
+    for name, payment in zip(names, payment):
+        zippedData[name] = payment
+    print(zippedData)
+    return zippedData
+
+
+def getNamesFromExpenses(expense):
+    names = []
+    for i in expense:
+        paymentList = expense.get(i)
+        for j in paymentList.get("payments"):
+            names.append(j)
+        break
+    return names
+
+
+def differntiateWhoCanPayWhom(zippedPaymentData, contriPayment):
+    plus = {}
+    minus = {}
+    for i in zippedPaymentData:
+        print(i)
+
+    return 0
 
 
 """
@@ -60,7 +93,7 @@ def calculateTotalPaymentFromEachMembers(expense, totalMembers):
 
 totalMembers = 4
 
-#expense = {471: [50, 100, 1, 320], 890: [80, 320, 400, 90]}
+# expense = {471: [50, 100, 1, 320], 890: [80, 320, 400, 90]}
 
 expense = {"petrol": {"bill": 471, "payments": {"a": 50, "b": 100, "c": 1, "d": 320}},
            "room": {"bill": 890, "payments": {"a": 80, "b": 320, "c": 400, "d": 90}}}
