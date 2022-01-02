a29c65bfab8e76885363d6efd65c165f6d7a3624
sats17 <satishkumbhar9999@gmail.com>
2022-01-02 17:48:10 +0530

    Refactored variable

--- ProblemSolving/SearchKeyWordInParagraph.py
+++ ProblemSolving/SearchKeyWordInParagraph.py
@@ -12,27 +12,27 @@ Time Complexity : O(len(paragraph))
 
 def getMatchedIndicesByComparingChars(paragraph, word):
     # Initialize index variable of paragraph and word
-    i = 0
-    j = 0
+    paragraphIndex = 0
+    wordIndex = 0
     matchedIndices = []  # Result storage
-    while i <= len(paragraph) - 1:
+    while paragraphIndex <= len(paragraph) - 1:
         # run loop on each character of paragraph
-        if paragraph[i].lower() != word[j].lower():
+        if paragraph[paragraphIndex].lower() != word[wordIndex].lower():
             # if comparison did not match between paragraph and word char, increasing paragraph iteration
-            i += 1
-            if j != 0:
+            paragraphIndex += 1
+            if wordIndex != 0:
                 # Doing this condition and setting index for word to initialize because we want to compare the word
                 # to paragraph from start
-                j = 0
+                wordIndex = 0
         else:
             # If chars matched then just moving to next iterator
-            i += 1
-            j += 1
-            if j > len(word) - 1:
+            paragraphIndex += 1
+            wordIndex += 1
+            if wordIndex > len(word) - 1:
                 # If word j index value is equal to word size then our total word is matched in paragraph
                 # and we are storing index in array
-                matchedIndices.append(i - len(word))
-                j = 0
+                matchedIndices.append(paragraphIndex - len(word))
+                wordIndex = 0
     print("matched  on index ", matchedIndices)
     return matchedIndices
 
