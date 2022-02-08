30ecda28ecee292b3544b4764e709d9f2444010a
sats17 <satishkumbhar9999@gmail.com>
2022-02-08 20:42:25 +0530

    Added conversion test

--- ProblemSolving/BitwiseUtility.py
+++ ProblemSolving/BitwiseUtility.py
@@ -7,5 +7,21 @@ def integerToBinary(n):
         result = str(answer) + result
         n = n // 2
     return result
+# Convert integer to byte array
+def integerToByteArray(n):
+    # Convert integer to binary string
+    binary = integerToBinary(n)
+    # Convert binary string to byte array
+    byteArray = []
+    for i in range(0, len(binary), 8):
+        byteArray.append(int(binary[i:i+8], 2))
+    return byteArray
 
-print(integerToBinary(13))
+a = "123"
+a_bytes = bytes(a, "ascii")
+for x in a_bytes:
+    print(type(x))
+    print("{0:b}".format(x))
+print(' '.join(["{0:b}".format(x) for x in a_bytes]))
+print(integerToByteArray(123))
+print(integerToBinary(1))
