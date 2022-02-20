783d68df47f2ec892d1a3eddecac3c72bd7b32ed
sats17 <satishkumbhar9999@gmail.com>
2022-02-20 14:12:47 +0530

    Added memoization logic for finding pairs

--- ProblemSolving/Pairs.py
+++ ProblemSolving/Pairs.py
@@ -1,7 +1,8 @@
 """
 Function Description
 
-Complete the pairs function below. It must return an integer representing the number of element pairs having the required difference.
+Complete the pairs function below. It must return an integer representing the number of element pairs 
+having the required difference.
 
 pairs has the following parameter(s):
 
@@ -10,7 +11,7 @@ pairs has the following parameter(s):
 
 Sample Input
 
-5 2
+2
 1 5 3 4 2
 
 Sample Output
@@ -25,19 +26,22 @@ def pairs(k, arr):
     arrLen = len(arr)
     for i in range(0,arrLen - 1):
         for j in range(i+1,arrLen):
+            l = [1, 3, 2, 5, 4]
+            # 1 + 2 = 3
             if(abs(arr[i] - arr[j]) == k):
+                # 1 - 2 = 2
                 count +=1
                 break
     return count
 
 def pairsAdvance(k, arr):
     arr.sort()
+    print("After sorted array is ",arr)
     arrLen = len(arr)
     nextIndex = 1
     currentIndex = 0
     count = 0
     while nextIndex < arrLen:
-        print("i")
         diff = arr[nextIndex] - arr[currentIndex]
 
         if diff == k:
@@ -50,10 +54,43 @@ def pairsAdvance(k, arr):
             nextIndex += 1
     return count
 
+def memoizedPairsDiff(k, arr):
+    """
+    In memoization logic, we are just putting the array values into hashset(o(1) Complexity))),
+    by doing this we are just doing sum of current iteration and k, if this value present in hashset. 
+    Then it contains the pair.
+    Unfortunately we required reverse sort if we want to find the pair of diff k.
+    arr = [9, 5, 3, 4, 2, 11]
+    """
+    print("Incoming array ",arr)
+    print("Required diff is ",k)
+    arr.sort(reverse=True) # sort in descending order, required if you are finding the pairs with diff k
+    memoizedArr = set()
+    count = 0
+    for i in arr:
+        print("i is ",i)
+        sum = i + k
+        if(sum in memoizedArr):
+            print("Found a pair ",i,"+",k,"=",sum)
+            count +=1
+            memoizedArr.add(i)
+        else:
+            memoizedArr.add(i)
+    print("Diff is ",k)
+    print("Array is ",arr)
+    print("Memoized array is ",memoizedArr)
+    return count
+
 if __name__ == "__main__":
-    arr = [4, 7, 8, 9, 11]
+    arr = [9, 5, 3, 4, 2, 7, 11]
     result = pairsAdvance(2,arr)
-    print(result)
+    # print("Result is ",pairs(2,arr))
+    print("pair advance result ",result)
+    print(memoizedPairsDiff(2,arr))
+    # driver code
+    # A = [9, 5, 3, 4, 2, 11]
+    # n = 2
+    # printPairs(A, len(A), n)
 """
     var
     i = 0, j = 1, count = 0;
