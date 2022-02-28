69db978723d5bc501da1f2ee5e1118eda527f944
sats17 <satishkumbhar9999@gmail.com>
2022-02-28 12:50:16 +0530

    Added logic detail for ugly number

--- ProblemSolving/UglyNumbers.py
+++ ProblemSolving/UglyNumbers.py
@@ -12,8 +12,17 @@ def isUgly(num):
     return num == 1
 # print(isUgly(1878))
 answer = []
-for i in range(1,50):
+for i in range(1,10):
     if isUgly(i): 
         answer.append(i)
-print(answer.__len__())
-print(answer[19])
+print(answer)
+# print(answer.__len__())
+# print(answer[19])
+"""
+Next action item:
+Following this https://medium.com/@interviewprep/ugly-number-dynamic-programming-94520110a084
+Working on logic of heap and min heap data structure.
+In above logic they are multiplying by 2,3,5 starting from 1. And when we have data of multiplication
+then by picking the minimum value of multiplication we can get the next values. also our minimum values
+are ugly numbers
+"""
