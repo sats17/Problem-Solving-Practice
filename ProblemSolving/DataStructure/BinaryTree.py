fd18d9a7cce0f7549c4463c2250e233fd07fcccf
sats17 <satishkumbhar9999@gmail.com>
2022-03-06 21:45:23 +0530

    Added binary tree schema

--- ProblemSolving/DataStructure/BinaryTree.py
+++ ProblemSolving/DataStructure/BinaryTree.py
@@ -1,17 +1,27 @@
+
+"""
+Need to do: Create a tree, which is a binary tree. It will add nodes to the tree.
+Simple condition, if value is less than the root, then it will go to the left. 
+If value is greater than the root, then it will go to the right.
+"""
 class BinaryTree:
     def __init__(self, size):
-        self.tree = []
+        self.tree = None
         return None
 
     def getTree(self):
         return self.tree
 
-    def put(self, value):
-        self.tree.append(value)
+    def put(self, value, test = False):
+        b = self.tree
+        self.tree = "hmm"
+        print(b)
+        print(self.tree)
+        # self.tree.append(value)
         return None
 
 if __name__ == "__main__":
     n = [5, 1, 2, 3, 4]
     bt = BinaryTree(1)
-    bt.put(5)
+    bt.put(5, True)
     print(bt.getTree())
