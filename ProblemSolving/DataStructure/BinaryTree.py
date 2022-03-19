eb639f64668d9d8cd94c098c10b95d18a4c28f34
sats17 <satishkumbhar9999@gmail.com>
2022-03-19 22:11:56 +0530

    Initialized tree

--- ProblemSolving/DataStructure/BinaryTree.py
+++ ProblemSolving/DataStructure/BinaryTree.py
@@ -4,24 +4,32 @@ Need to do: Create a tree, which is a binary tree. It will add nodes to the tree
 Simple condition, if value is less than the root, then it will go to the left. 
 If value is greater than the root, then it will go to the right.
 """
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 class BinaryTree:
-    def __init__(self, size):
+    def __init__(self):
         self.tree = None
         return None
 
     def getTree(self):
         return self.tree
 
-    def put(self, value, test = False):
-        b = self.tree
-        self.tree = "hmm"
-        print(b)
-        print(self.tree)
-        # self.tree.append(value)
-        return None
+    def insert(self, value):
+        if self.tree is None:
+            self.tree = Node(value)
+        else:
+            self._insert(value, self.tree)
+    
+    def _insert(self, value, node):
+        print("TEst")
+
 
 if __name__ == "__main__":
-    n = [5, 1, 2, 3, 4]
-    bt = BinaryTree(1)
-    bt.put(5, True)
-    print(bt.getTree())
+    tree = BinaryTree()
+    tree.insert(2)
+    print(tree.getTree())
