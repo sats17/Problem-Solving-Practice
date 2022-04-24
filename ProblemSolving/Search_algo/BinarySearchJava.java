a270de2c987d183bb256fb3686fe71fcceedd5ec
sats17 <satishkumbhar9999@gmail.com>
2022-04-24 23:14:14 +0530

    Added insert index At logic

--- ProblemSolving/Search_algo/BinarySearchJava.java
+++ ProblemSolving/Search_algo/BinarySearchJava.java
@@ -18,17 +18,41 @@ public class BinarySearchJava {
             int newMidIndex = newArray.length / 2;
             return search(newArray, newMidIndex, value);
         }
+    }
 
+    public static int[] insertAtIndex(int[] array, int index, int value) {
+        int[] newArray = new int[array.length + 1];
+        int currentIndex = 0;
+        int currentIndexOfArray = 0;
+        while(currentIndex < array.length) {
+            if(currentIndex == index) {
+                newArray[index] = value;
+                currentIndexOfArray++;
+                index = -1;
+            } else {
+                newArray[currentIndexOfArray] = array[currentIndex];
+                currentIndex++;
+                currentIndexOfArray++;
+            }
+        }
+        return newArray;
     }
 
     public static void main(String[] args) {
         // int val = 10 / 3;
         // System.out.println(val);
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(search(arr, arr.length / 2, 1));
-        for(int i = 0; i <= 19; i++) {
-            System.out.println(search(arr, arr.length / 2, i));
-        }
+        int[] updatedArr = insertAtIndex(arr, 3, 11);
+        System.out.println(Arrays.toString(updatedArr));
+        // System.out.println(search(arr, arr.length / 2, 1));
+        // for(int i = 0; i <= 19; i++) {
+        //     System.out.println(search(arr, arr.length / 2, i));
+        // }
+        /**
+         * while() {
+         *  
+         * }
+         */
         
     }
 }
