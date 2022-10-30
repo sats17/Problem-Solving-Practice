e14f9ee494b59b57f31a5fa6b284adaecc7e2a96
sats17 <satishkumbhar9999@gmail.com>
2022-10-30 11:16:05 +0530

    Added array split

--- ProblemSolving/split_array.py
+++ ProblemSolving/split_array.py
@@ -1,22 +1,23 @@
+import numpy as np
+import math
+
+
 def split(array, size):
-    divider = len(array) // size
-    print(len(array))
-    print(divider)
+    resp_list = []
+    divider = math.ceil(len(array) / size)
     temp_divider = 0
     for i in range(size):
-        print("temp_divider ",temp_divider)
         if i == size - 1:
-            print(array[temp_divider:])
+            resp_list.append(array[temp_divider:])
         else:
-            print(array[temp_divider:temp_divider + divider])
+            resp_list.append(array[temp_divider:temp_divider + divider])
         temp_divider += divider
-    # for i in range(0, len(array) - 1, divider):
-    #     print(array[i])
-    #     print(i)
-    #     print(divider)
-    #     print(array[i: i + divider])
-
+    return resp_list
 
 if __name__ == '__main__':
-    arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u']
-    split(arr, 8)
+    # arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q']
+    arr = list(range(1, 120))
+    print("generated array ", arr)
+    resp = split(arr, 45)
+    for i in resp:
+        print(i)
