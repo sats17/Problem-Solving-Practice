d2bab81eb6631cfc31b455aee4f59e72d6742c69
Satish Kumbhar <40035899+sats17@users.noreply.github.com>
2023-10-01 06:54:16 +0000

    Added graph

--- ProblemSolving/DataStructure/Graph/Graph.py
+++ ProblemSolving/DataStructure/Graph/Graph.py
@@ -2,12 +2,40 @@
 class Node:
     def __init__(self, node):
         self.id = node
-        self.adjacent = {}
+        self.neighbour = {}
+    
+    def set_neighbour(self, neighbour:Node):
+        self.neighbour[neighbour.get_node_id()] = neighbour
+
+    def __hash__(self):
+        return hash(self.id)
+    
+    def get_node_id(self):
+        return self.id
+    
+    def __str__(self):
+        return f"Node instance with id={self.id} and neighbour={self.neighbour}"
 
 class Graph:
     def __init__(self):
         self.node_dict = {}
         self.num_nodes = 0
 
+    def set_node(self, node:Node):
+        if node.get_node_id() not in self.node_dict:
+            self.node_dict[node.get_node_id()] = node
+        else:
+            print("Node already present, cannot add")
+    
+    def get_nodes(self):
+        for key, value in self.node_dict.items():
+            print(f'Node ID: {key}, Value: {value.__str__()}')
+    
+    def set_neighbour(self, )
+
 if __name__ == '__main__':
     print("Hi there")
+    graph = Graph()
+    node1 = Node(1)
+    graph.set_node(node1)
+    graph.get_nodes()
