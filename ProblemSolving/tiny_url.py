badf30355a9bb7c30c03c8ee20493a32d4e96ca8
Satish Kumbhar <40035899+sats17@users.noreply.github.com>
2023-10-29 14:28:05 +0530

    Update tiny_url.py

--- ProblemSolving/tiny_url.py
+++ ProblemSolving/tiny_url.py
@@ -4,4 +4,4 @@
 # To convert base10 to base62, divide base10 by 62 and collect the remainders until the quotient becomes zero.
 # Convert remainders to base62 equivalent value example Z = 61, 1 = 1, 0 = 0. Base 62 starts index from 0 to 61 with mapping
 # as 0,1,2...Y,Z.
-
+# While converting to tiny-url, take last remainder as first value
