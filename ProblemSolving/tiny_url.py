64e1c26db5cf40f6f5bdf18c58ab293448492b65
Satish Kumbhar <40035899+sats17@users.noreply.github.com>
2023-10-29 15:33:57 +0530

    Update tiny_url.py

--- ProblemSolving/tiny_url.py
+++ ProblemSolving/tiny_url.py
@@ -5,3 +5,27 @@
 # Convert remainders to base62 equivalent value example Z = 61, 1 = 1, 0 = 0. Base 62 starts index from 0 to 61 with mapping
 # as 0,1,2...Y,Z.
 # While converting to tiny-url, take last remainder as first value
+
+
+import hashlib
+
+def hash_string(input_string):
+    # Encode the string to bytes (UTF-8 encoding is commonly used)
+    encoded_string = input_string.encode('utf-8')
+    
+    # Create a SHA-256 hash object
+    sha256_hash = hashlib.sha256()
+    
+    # Update the hash object with the encoded bytes
+    sha256_hash.update(encoded_string)
+    
+    # Get the hexadecimal representation of the hash
+    hashed_string = sha256_hash.hexdigest()
+    
+    return hashed_string
+
+# Example usage
+input_string = "Hello, World!"
+hashed_string = hash_string(input_string)
+
+print(f"The hash of '{input_string}' is: {hashed_string}")
